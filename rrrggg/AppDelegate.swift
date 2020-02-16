2bd8dd485687211aeadf2151c08b1b70ef2fe933
jbc4792 <60057022+jbc4792@users.noreply.github.com>
2020-02-15 22:40:58 -0500

    Initial Commit

---
+++ rrrggg/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  rrrggg
+//
+//  Created by Josh Collins on 2/15/20.
+//  Copyright © 2020 Josh Collins. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ rrrggg/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  rrrggg
+//
+//  Created by Josh Collins on 2/15/20.
+//  Copyright © 2020 Josh Collins. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
